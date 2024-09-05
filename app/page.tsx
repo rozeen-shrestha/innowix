c14f69797ea524b69c1b3a07896b9c09d7109385
rozeeen-shrestha <rozeeen.shrestha@gmail.com>
2024-09-05 10:45:01 +0000

    initial

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { HeaderMegaMenu } from "../components/headermenu/HeaderMegaMenu";
+import { HeroMain } from "../components/HeroMain/HeroMain";
+
+
+export default function HomePage() {
+  return <div>
+<HeaderMegaMenu/>
+<HeroMain/>
+  </div>;
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+      // If you have other directories with Tailwind CSS classes, include them here
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
